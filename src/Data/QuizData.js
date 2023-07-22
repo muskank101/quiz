961da785549fdbd1ed8768cb50c9c289e2da71a1
Muskan Koshti <koshtichicku1399@gmail.com>
2023-07-22 16:46:40 +0530

    initial commit

---
+++ src/Data/QuizData.js
@@ -0,0 +1,27 @@
+export const QuizData = [
+    {
+        question: "How many minutes are usually played in a football match ?",
+        options:[ "30", "45", "90", "120"],
+        answer: 3
+    },
+    {
+        question: "The spot kick is taken from where ?",
+        options:["The centre spot", "The Penalty spot", "The Ink spot", "On the spot"],
+        answer: 2
+    },
+    {
+        question: "Who won the 1998 World Cup Final?",
+        options: ["Germany", "France", "England", "Argentina"],
+        answer: 4
+    },
+    {
+        question: "In what color strip do North Korea play in?",
+        options: ["Blue", "White", "Red", "Yellow"],
+        answer:2
+    },
+    {
+        question: "Who won the European Cup in 1970?",
+        options: ["PSV Eindhoven", "Ajax", "Feyenoord", "Argentina"],
+        answer:3
+    },
+];
